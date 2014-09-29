cb1b15f660ebda28a5cfb5e6e2fc3aa6e8eed889
nordinsalam <nordinsalam@users.noreply.github.com>
2014-09-29 19:24:37 +0700

    Update icecast.php

--- icecast.php
+++ icecast.php
@@ -1,6 +1,7 @@
 <?php
-$SERVER = 'http://utyfmedari.us.radiokita.net:8000'; // url to icecast server and port
-$STATS_FILE = '/status3.xsl?mount=/utyfmedari'; // path to status3.xsl and mountpoint
+$SERVER = 'http://icecast.server:port'; // url to icecast server and port
+$STATS_FILE = '/icecast-status.xsl?mount=/mountpoint'; // path to icecast-status.xsl and mountpoint
+$STREAM_PAGE = 'http://streampage'; // url to streaming web page or homepage
 
 // END CONFIGURATION
 
@@ -19,15 +20,16 @@ $output = curl_exec($ch);
 //close curl resource to free up system resources 
 curl_close($ch); 
 
-#$output = "-/utyfmedari,260,12,21,48,Katy Perry - This Is How We Do,";
+#$output = "90.7 UTYFM Medari,1,Armada - Katakan Sejujurnya";
 #print_r (explode(",",$output));
 
 $temp_array = (explode(",",$output));
 
 //sort our temp array into our ral array 
-$radio_info['listeners'] = $temp_array[2]; 
-$radio_info['url'] = "kupingin.utyfmedari.com"; 
-$radio_info['track'] = $temp_array[5]; 
+$radio_info['station_name'] = $temp_array[1];
+$radio_info['listener'] = $temp_array[2];
+$radio_info['url'] = $STREAM_PAGE; 
+$radio_info['track'] = $temp_array[3]; 
 $data['song'] = $radio_info['track'];
 $data['complete'] = '#np '. $data['song'] .' on '. $radio_info['url'] ;
 
